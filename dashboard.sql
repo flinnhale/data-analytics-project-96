24a8b22cda6f81576d660e69ab5b2161ce37310c
Aleksandra <sorowless13@gmail.com>
2024-12-19 15:28:36 +0300

    add new query

--- dashboard.sql
+++ dashboard.sql
@@ -213,3 +213,81 @@ select
         order by date_trunc('day', created_at - visit_date)
     ) as days_for_close
 from tab2;
+
+-- окупаемость каналов
+
+with tab1 as (
+    select
+        s.visitor_id,
+        date(s.visit_date) as visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        row_number()
+            over (
+                partition by s.visitor_id
+                order by s.visit_date desc
+            )
+        as rn
+    from sessions as s
+    left join leads as l on s.visitor_id = l.visitor_id
+    where s.medium in ('cpc', 'cpm', 'cpa', 'cpp', 'tg', 'youtube', 'social')
+),
+
+last_paid_click as (
+    select
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        count(visitor_id) as visitors_count,
+        count(lead_id) as leads_count,
+        visit_date,
+        count(status_id) filter (where status_id = 142) as purchases_count,
+        sum(amount) as revenue
+    from tab1
+    where rn = 1
+    group by 6, 1, 2, 3
+),
+
+ads as (
+
+    select
+        date(campaign_date) as campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_spent
+    from vk_ads
+    group by 1, 2, 3, 4
+    union
+    select
+        date(campaign_date) as campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_spent
+    from ya_ads
+    group by 1, 2, 3, 4
+    order by 1
+)
+
+select
+    lpc.utm_source,
+    sum(total_spent) as total_spent,
+    sum(revenue) as total_revenue,
+    sum(revenue) - sum(total_spent) as profit
+from last_paid_click as lpc
+left join ads
+    on
+        lpc.utm_source = ads.utm_source
+        and lpc.utm_medium = ads.utm_medium
+        and lpc.utm_campaign = ads.utm_campaign
+        and lpc.visit_date = ads.campaign_date
+where lpc.utm_source in ('yandex', 'vk')
+group by 1
+order by 1 desc nulls last;
